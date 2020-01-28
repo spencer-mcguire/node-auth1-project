a4fb3acdfccfa2e6b3ffc860e3ab03533e2d89fe
spencer-mcguire <smcguire1826@gmail.com>
2020-01-28 13:46:51 -0600

    configure middleware for server

--- api/configure-middleware.js
+++ api/configure-middleware.js
@@ -1,9 +1,33 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session);
+
+const dbConnection = require('../data/dbConfig');
+
+const sessionConfig = {
+  name: 'spencersCookie',
+  secret: process.env.SESSION_SECRET || 'keep it secret, keep it safe',
+  cookie: {
+    maxAge: 1000 * 60 * 10, // 10 mins in ms
+    secure: false,
+    httpOnly: true
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new KnexSessionStore({
+    knex: dbConnection,
+    tablename: 'sessions',
+    sidfieldname: 'sid',
+    createtable: true,
+    clearInterval: 60000
+  })
+};
 
 module.exports = server => {
   server.use(helmet());
+  server.use(session(sessionConfig));
   server.use(express.json());
   server.use(cors());
 };
